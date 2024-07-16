55295743620b61e8e6cefd1e63ca8dfed5ce7230
Alex Antrim - 957 <elixantrim@gmail.com>
2024-07-15 19:02:10 -0700

    Added more descriptive comments.

---
+++ main/scroll_text.h
@@ -0,0 +1,9 @@
+#ifndef SCROLL_TEXT_H
+#define SCROLL_TEXT_H
+
+void drawSponsors (void);
+void drawScrolltext(String scrolltextString);
+void drawDeclarationFull (void);
+void drawDeclaration (int times);
+
+#endif
